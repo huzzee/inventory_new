efa596d8239241a0eae329fafea171f1aac01e66
huzzee <30508545+huzzee@users.noreply.github.com>
2017-10-07 16:26:31 +0500

    database

--- my_invento.sql
+++ my_invento.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 07, 2017 at 10:42 AM
+-- Generation Time: Oct 07, 2017 at 11:26 AM
 -- Server version: 5.7.11
 -- PHP Version: 7.1.7
 
@@ -19,6 +19,35 @@ SET time_zone = "+00:00";
 --
 -- Database: `my_invento`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_categories`
+--
+
+CREATE TABLE `item_categories` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `item_cat_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `item_type_id` int(10) UNSIGNED NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item_types`
+--
+
+CREATE TABLE `item_types` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `item_type_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -51,7 +80,10 @@ INSERT INTO `menus` (`id`, `menu_name`, `menu_slug`, `parent_menu_id`, `order`, 
 (9, 'Users List', 'users', 7, 2, NULL, 'users.index', 1, 0, 2, NULL, NULL),
 (10, 'Edit', NULL, 7, 3, NULL, 'users.edit', 1, 1, 4, NULL, NULL),
 (11, 'Users Profile', NULL, 7, 4, NULL, 'users.show', 1, 1, 5, NULL, NULL),
-(12, 'Delete', NULL, 7, 5, NULL, 'users.delete', 1, 1, 6, NULL, NULL);
+(12, 'Delete', NULL, 7, 5, NULL, 'users.delete', 1, 1, 6, NULL, NULL),
+(13, 'Items/Products', NULL, NULL, 0, 'mdi mdi-collection-image-o', NULL, 1, 0, 7, NULL, NULL),
+(14, 'Item Type', 'item_types', 13, 1, NULL, 'item_types.index', 1, 0, 8, NULL, NULL),
+(15, 'Item Category', 'item_categories', 13, 2, NULL, 'item_categories.index', 1, 0, 9, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -73,7 +105,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2017_10_06_200021_create_roles_table', 1),
-(4, '2017_10_07_092102_create_menus_table', 2);
+(4, '2017_10_07_092102_create_menus_table', 2),
+(6, '2017_10_07_110820_create_item_types_table', 3),
+(7, '2017_10_07_110933_create_item_categories_table', 3);
 
 -- --------------------------------------------------------
 
@@ -141,6 +175,19 @@ INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `role_id`, `pa
 --
 
 --
+-- Indexes for table `item_categories`
+--
+ALTER TABLE `item_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_categories_item_type_id_foreign` (`item_type_id`);
+
+--
+-- Indexes for table `item_types`
+--
+ALTER TABLE `item_types`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `menus`
 --
 ALTER TABLE `menus`
@@ -176,15 +223,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `item_categories`
+--
+ALTER TABLE `item_categories`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `item_types`
+--
+ALTER TABLE `item_types`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `roles`
 --
@@ -195,6 +252,16 @@ ALTER TABLE `roles`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `item_categories`
+--
+ALTER TABLE `item_categories`
+  ADD CONSTRAINT `item_categories_item_type_id_foreign` FOREIGN KEY (`item_type_id`) REFERENCES `item_types` (`id`) ON DELETE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
