35ba702a147dd8b387a456ebb3896874a6b14e05
huzzee <30508545+huzzee@users.noreply.github.com>
2017-10-07 18:36:37 +0500

    items

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,21 @@ Route::get('/', 'HomeController@index')->name('home');
 				    Resources Route
 ------------------------------------------------------*/
 
-Route::Resource('menus','MenuController');
-Route::Resource('users','UserController');
-Route::Resource('item_types','Item_typeController');
-Route::Resource('item_categories','Item_categoryController');
+Route::Resource('menus','MenuController');// for menus
+Route::Resource('users','UserController');// for manage users
+Route::Resource('items','ItemController');// for manage items
+Route::Resource('item_types','Item_typeController');//item Types
+Route::Resource('item_categories','Item_categoryController');//item categories
 
 /*----------------------------------------------------
 				    Resources Route
+------------------------------------------------------*/
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+/*----------------------------------------------------
+				    Ajax Routes
+------------------------------------------------------*/
+
+/*----------------------------------------------------
+				    Ajax Routes
 ------------------------------------------------------*/
