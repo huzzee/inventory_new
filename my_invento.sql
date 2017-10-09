4575df62c699fd8863820084850b7a9635d9223c
huzzee <30508545+huzzee@users.noreply.github.com>
2017-10-09 15:47:29 +0500

    database
    isme mene user name admin kardiya he or password:password he

--- my_invento.sql
+++ my_invento.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 07, 2017 at 11:26 AM
+-- Generation Time: Oct 09, 2017 at 10:46 AM
 -- Server version: 5.7.11
 -- PHP Version: 7.1.7
 
@@ -23,17 +23,50 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `catagory_id` int(10) UNSIGNED NOT NULL,
+  `type_id` int(10) UNSIGNED NOT NULL,
+  `item_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci,
+  `item_unit` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `opening_qnt` int(11) NOT NULL DEFAULT '0',
+  `current_qnt` int(11) NOT NULL DEFAULT '0',
+  `min_qnt` int(11) NOT NULL DEFAULT '0',
+  `item_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `unit_price` double(7,2) NOT NULL DEFAULT '0.00',
+  `discount_price` double(7,2) NOT NULL DEFAULT '0.00',
+  `discount_percent` tinyint(4) NOT NULL DEFAULT '0',
+  `is_saleable` tinyint(1) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `sort_order` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `item_categories`
 --
 
 CREATE TABLE `item_categories` (
   `id` int(10) UNSIGNED NOT NULL,
   `item_cat_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `item_type_id` int(10) UNSIGNED NOT NULL,
   `status` tinyint(1) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `item_categories`
+--
+
+INSERT INTO `item_categories` (`id`, `item_cat_name`, `status`, `created_at`, `updated_at`) VALUES
+(2, 'Test', 1, '2017-10-08 09:16:01', '2017-10-08 09:16:01');
 
 -- --------------------------------------------------------
 
@@ -48,6 +81,14 @@ CREATE TABLE `item_types` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `item_types`
+--
+
+INSERT INTO `item_types` (`id`, `item_type_name`, `status`, `created_at`, `updated_at`) VALUES
+(2, 'General Items', 1, '2017-10-08 08:36:29', '2017-10-08 08:36:29'),
+(3, 'Raw Material', 1, '2017-10-08 09:14:07', '2017-10-08 09:14:07');
 
 -- --------------------------------------------------------
 
@@ -81,9 +122,11 @@ INSERT INTO `menus` (`id`, `menu_name`, `menu_slug`, `parent_menu_id`, `order`, 
 (10, 'Edit', NULL, 7, 3, NULL, 'users.edit', 1, 1, 4, NULL, NULL),
 (11, 'Users Profile', NULL, 7, 4, NULL, 'users.show', 1, 1, 5, NULL, NULL),
 (12, 'Delete', NULL, 7, 5, NULL, 'users.delete', 1, 1, 6, NULL, NULL),
-(13, 'Items/Products', NULL, NULL, 0, 'mdi mdi-collection-image-o', NULL, 1, 0, 7, NULL, NULL),
-(14, 'Item Type', 'item_types', 13, 1, NULL, 'item_types.index', 1, 0, 8, NULL, NULL),
-(15, 'Item Category', 'item_categories', 13, 2, NULL, 'item_categories.index', 1, 0, 9, NULL, NULL);
+(13, 'Items/Products', NULL, NULL, 0, 'mdi mdi-clipboard-text', NULL, 1, 0, 7, NULL, NULL),
+(14, 'Item Type', 'item_types', 13, 1, NULL, 'item_types.index', 1, 0, 10, NULL, NULL),
+(15, 'Item Category', 'item_categories', 13, 2, NULL, 'item_categories.index', 1, 0, 11, NULL, NULL),
+(16, 'Add Items', 'items/create', 13, 4, NULL, 'items.create', 1, 0, 8, NULL, NULL),
+(17, 'Items List', 'items', 13, 5, NULL, 'items.index', 1, 0, 9, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -106,8 +149,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2017_10_06_200021_create_roles_table', 1),
 (4, '2017_10_07_092102_create_menus_table', 2),
-(6, '2017_10_07_110820_create_item_types_table', 3),
-(7, '2017_10_07_110933_create_item_categories_table', 3);
+(10, '2017_10_07_110820_create_item_types_table', 3),
+(13, '2017_10_07_110933_create_item_categories_table', 4),
+(15, '2017_10_07_133115_create_items_table', 5);
 
 -- --------------------------------------------------------
 
@@ -168,18 +212,25 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `role_id`, `password`, `profile_image`, `status`, `gender`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Huzaifa', 'Siddiqui', 'huzaifa24', 1, '$2y$10$QsvoKLxQZfJXIC1XDuaYReoE/r78WwCyLPc1kzLzOf5irIxhf0h3K', NULL, 1, 1, 'my7HWW4DbYSdCqmsRFzhkMGScdGO2oEgCrwa9YWTTLb4Ts0GCcuRDxcAbHAc', NULL, NULL);
+(1, 'Huzaifa', 'Siddiqui', 'admin', 1, '$2y$10$QsvoKLxQZfJXIC1XDuaYReoE/r78WwCyLPc1kzLzOf5irIxhf0h3K', NULL, 1, 1, 'jfExjscTi5f1Fm9qc6OCb5OzT2XvdTvTAiBRzBrakzdRyTonUKglA0lwthxf', NULL, NULL);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `items_catagory_id_foreign` (`catagory_id`),
+  ADD KEY `items_type_id_foreign` (`type_id`);
+
+--
 -- Indexes for table `item_categories`
 --
 ALTER TABLE `item_categories`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `item_categories_item_type_id_foreign` (`item_type_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `item_types`
@@ -223,25 +274,30 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `item_categories`
 --
 ALTER TABLE `item_categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `item_types`
 --
 ALTER TABLE `item_types`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT for table `roles`
 --
@@ -257,10 +313,11 @@ ALTER TABLE `users`
 --
 
 --
--- Constraints for table `item_categories`
+-- Constraints for table `items`
 --
-ALTER TABLE `item_categories`
-  ADD CONSTRAINT `item_categories_item_type_id_foreign` FOREIGN KEY (`item_type_id`) REFERENCES `item_types` (`id`) ON DELETE CASCADE;
+ALTER TABLE `items`
+  ADD CONSTRAINT `items_catagory_id_foreign` FOREIGN KEY (`catagory_id`) REFERENCES `item_categories` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `items_type_id_foreign` FOREIGN KEY (`type_id`) REFERENCES `item_types` (`id`) ON DELETE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
