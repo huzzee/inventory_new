cd6f759f89ba690d600063f44156e8a8d7fe4e52
huzzee <30508545+huzzee@users.noreply.github.com>
2017-10-16 14:35:57 +0500

    model update

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Supplier extends Model
 {
     protected $fillable = [
-    	'sup_name','sup_phone','sup_address','sup_email','sup_image','status'
+    	'sup_name','sup_phone','sup_address','sup_email','sup_image','status','sup_account'
     ];
 
     
